b57127a0988da6ebb8550038dee0c6101a1eab7b
Hill <125409221+NARI1108@users.noreply.github.com>
2023-07-14 13:38:56 +0330

    Creating a bridge between the XML layer and the Java layer through ID

--- app/src/main/java/com/example/counttheblessings/MainActivity.java
+++ app/src/main/java/com/example/counttheblessings/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.counttheblessings;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView txt_count;
+    Button btn_reset, btn_blessing;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+    }
+    public void findViews(){
+        txt_count = findViewById(R.id.txt_count);
+        btn_blessing = findViewById(R.id.btn_blessing);
+        btn_reset = findViewById(R.id.btn_reset);
     }
 }
