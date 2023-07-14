e19cd6b944e6a9cf784d34a88f1fd7cb8f7ca12b
Hill <125409221+NARI1108@users.noreply.github.com>
2023-07-14 15:13:39 +0330

    Creating animations to beautify the graphic design of our application.

--- app/src/main/java/com/example/counttheblessings/MainActivity.java
+++ app/src/main/java/com/example/counttheblessings/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.counttheblessings;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,10 +16,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViews();
+        animation();
     }
     public void findViews(){
         txt_count = findViewById(R.id.txt_count);
         btn_blessing = findViewById(R.id.btn_blessing);
         btn_reset = findViewById(R.id.btn_reset);
+    }
+    public void animation(){
+        Animation txt_count_Animation = AnimationUtils.loadAnimation(this,R.anim.alpha_animation);
+        Animation btn_blessing_Animation = AnimationUtils.loadAnimation(this,R.anim.trans_left_animation);
+        Animation btn_reset_Animation = AnimationUtils.loadAnimation(this,R.anim.trans_right_animation);
+
+        txt_count.setAnimation(txt_count_Animation);
+        btn_blessing.setAnimation(btn_blessing_Animation);
+        btn_reset.setAnimation(btn_reset_Animation);
     }
 }
